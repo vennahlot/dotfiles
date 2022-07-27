0b4442727af501c466b91fb203fff11aaf5ae68d
Yichen Wang <vennahlot@users.noreply.github.com>
2022-07-27 16:28:50 -0700

    Add current configs for Neovim

---
+++ neovim/lua/vennahlot/colorscheme.lua
@@ -0,0 +1,10 @@
+local colorscheme = "gruvbox"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+vim.g.gruvbox_contrast_dark = 'hard'
+
